a2f1fcc914d5b1dc3d84b6fd7c97e7713c7466fb
DOREMON <109501363+bhausetup@users.noreply.github.com>
2023-05-15 00:53:22 +0530

    Update help.py

--- Romeo/modules/help/help.py
+++ Romeo/modules/help/help.py
@@ -25,7 +25,8 @@ async def module_help(client: Client, message: Message):
     if len(cmd) > 1:
         help_arg = " ".join(cmd[1:])
     elif not message.reply_to_message and len(cmd) == 1:
-        await message.edit("⚡️")
+        await message.edit("👿")
+                         
         try:
             nice = await client.get_inline_bot_results(bot=bot_username, query="helper")
             await asyncio.gather(
@@ -44,7 +45,7 @@ async def module_help(client: Client, message: Message):
                 ac.add_row([x[0], x[1] if len(x) >= 2 else None])
             xx = await client.send_message(
                 message.chat.id,
-                f"```{str(ac)}```\n• @RomeoBot_op × @Romeo_op •",
+                f"```{str(ac)}```\n•  × @DRACULA_CHEERY •",
                 reply_to_message_id=ReplyCheck(message),
             )
             await xx.reply(
@@ -58,7 +59,7 @@ async def module_help(client: Client, message: Message):
             this_command = f"──「 **Help For {str(help_arg).upper()}** 」──\n\n"
             for x in commands:
                 this_command += f"  •  **Command:** `.{str(x)}`\n  •  **Function:** `{str(commands[x])}`\n\n"
-            this_command += "😎 @Romeo_op 😎"
+            this_command += "💓 @DRACULA_CHEERY 💓"
             await edit_or_reply(
                 message, this_command, parse_mode=enums.ParseMode.MARKDOWN
             )
@@ -97,7 +98,7 @@ async def module_helper(client: Client, message: Message):
             this_command = f"──「 **Help For {str(help_arg).upper()}** 」──\n\n"
             for x in commands:
                 this_command += f"  •  **Command:** `.{str(x)}`\n  •  **Function:** `{str(commands[x])}`\n\n"
-            this_command += "😎 @Romeo_op 😎"
+            this_command += "😟 @DRACULA_CHEERY 😟"
             await edit_or_reply(
                 message, this_command, parse_mode=enums.ParseMode.MARKDOWN
             )
